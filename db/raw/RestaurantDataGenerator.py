d06dc06cfedbbdae13dfcb27568f59ff464d2028
Shantanu Goel <shantanu.goel@gmail.com>
2018-03-25 13:47:04 +0530

    Add restaurant data

---
+++ db/raw/RestaurantDataGenerator.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import json
+import random
+
+# generate random cabs data
+
+f = open("restaurant_names.txt","r")
+restaurantNames = f.readlines()
+f.close()
+
+f = open("../../entities/cost.dat", "r")
+costList = f.readlines()
+f.close()
+
+f = open("../../entities/cuisine.dat", "r")
+cuisineList = f.readlines()
+f.close()
+
+f = open("../../entities/location.dat", "r")
+locList = f.readlines()
+f.close()
+
+
+data = []
+for i in range(200):
+    name = random.choice(restaurantNames).rstrip('\n')
+    cuisine = random.choice(cuisineList).rstrip('\n')
+    cost = random.choice(costList).rstrip('\n')
+    location = random.choice(locList).rstrip('\n')
+    data.append({
+        'name': name,
+        'cuisine': cuisine,
+        'cost': cost,
+        'location': location
+    })
+
+with open('./restaurant_data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
